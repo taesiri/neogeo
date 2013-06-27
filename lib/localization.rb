1e45a2e1b27c9961b3d246134863562cb4c0b1bd
taesiri <taesiri@live.com>
2013-06-27 20:10:17 +0430

    Multiple Changes

--- lib/localization.rb
+++ lib/localization.rb
@@ -3,22 +3,31 @@ def language_code_of(item)
   (item.identifier.match(/^\/([a-z]{2})\//) || [])[1]
 end
 
+def language_name_of(item)
+  language_name_for_code(
+      language_code_of(item))
+end
+
 def translations_of(item)
   @items.select do |i|
     i[:canonical_identifier] == item[:canonical_identifier]
   end
 end
 
-LANGUAGE_CODE_TO_NAME_MAPPING = {
-    'en' => 'English',
-    'fa' => 'Persian'
-}
-
 def language_name_for_code(code)
   LANGUAGE_CODE_TO_NAME_MAPPING[code]
 end
 
-def language_name_of(item)
-  language_name_for_code(
-      language_code_of(item))
+def get_translation_path
+  if @item[:translate_path] != nil
+    @item[:translate_path]
+  else
+    '(none)'
+  end
 end
+
+
+LANGUAGE_CODE_TO_NAME_MAPPING = {
+    'en' => 'English',
+    'fa' => 'Persian'
+}
